083e3ce8a6713fe16596c2ffd941f409814eb4b8
masao <masakiyama1218@outlook.jp>
2022-10-26 16:19:03 +0900

    header

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -6,16 +6,12 @@
         <a href="#" class="brand">MARUOKUN</a>
         </div>
 
-    <div class="menuHeader">
-          <ul class="navWrapper">
-        
-            <li><a href="#" class="menuItem">カレンダー</li></a>
-            <li><a href="#" class ="menuItem">従業員管理</li></a>
-            <li><a href="#" class="menuItem">通知管理</li></a>
-            <li><a href="#" class="menuItem">提出シフト管理</li></a>
-            <li><a href="#" class="menuItem">シフト閲覧</li></a>
-            <li><a href="#" class="menuItem">シフト作成</li></a>
-            
-          </ul>
-    </div>
+        <div class="menuHeaderWrap">
+            <a href="#" class="menuItem">カレンダー</a>
+            <a href="#" class ="menuItem">従業員管理</a>
+            <a href="#" class="menuItem">通知管理</a>
+            <a href="#" class="menuItem">提出シフト管理</a>
+            <a href="#" class="menuItem">シフト閲覧</a>
+            <a href="#" class="menuItem">シフト作成</a>
+         </div>
     </header>
