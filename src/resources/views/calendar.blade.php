d847d3e4ab64dbc34ff27f4ddaea6647430b97f0
shomamama <saokrt1416@au.com>
2022-10-26 15:02:47 +0900

    calendar

--- src/resources/views/calendar.blade.php
+++ src/resources/views/calendar.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/css/calendar.css" type="text/css">
       <div>
-      <iframe src="https://calendar.google.com/calendar/embed?src=tsumagari.shoma16%40gmail.com&ctz=Asia%2FTokyo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <iframe class="calendar" src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showPrint=0&showTabs=1&showTz=0&showCalendars=0&showDate=1&showNav=1&showTitle=1&mode=MONTH&src=dHN1bWFnYXJpLnNob21hMTZAZ21haWwuY29t&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043" style="border:solid 1px #777" width="1000" height="800" frameborder="0" scrolling="no"></iframe>
       </div>
     </header>
