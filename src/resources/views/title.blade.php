a960cfa700a944fba16e09a2acafaf4a99914fe2
masao <masakiyama1218@outlook.jp>
2022-10-28 16:17:19 +0900

    title.php css 追加

---
+++ src/resources/views/title.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="/css/title.css" type="text/css">
+<header>
+    <div class="blandWrapper">
+        <a href="#" class="brand">M&emsp;A&emsp;R&emsp;U&emsp;O&emsp;K&emsp;U&emsp;N</a>
+    </div>
+
+    <body>
+        <div class="titleName">
+            <a href="#" class="title">M&emsp;A&emsp;R&emsp;U&emsp;O&emsp;K&emsp;U&emsp;N</a>
+        </div>
+
+        <div class="ButtonArea">
+            <a href="#" class="loginButton">ログインはこちら</a><br />
+            <br />
+            <a href="#" class="registerButton">管理者の新規登録はこちら</a>
+        </div>
+    </body>
+</header>
