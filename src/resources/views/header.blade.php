c46ad325cc26763a3023a8391f16ddb4dd4de714
hkr910 <2001kudou0902@gmail.com>
2022-10-31 14:14:08 +0900

    welcome.blade.phpの大部分削除と、header.blade.phpの追記

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -21,18 +21,73 @@
             </li>
             <li>
               <a href="#" >従業員管理</a>
+              <ul>
+                <li>
+                  <a href="#" >編集</a>
+                </li>
+                <li>
+                  <a href="#" >閲覧</a>
+                </li>
+                <li>
+                  <a href="#" >(仮)</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="#" >通知管理</a>
+              <ul>
+                <li>
+                  <a href="#" >編集</a>
+                </li>
+                <li>
+                  <a href="#" >閲覧</a>
+                </li>
+                <li>
+                  <a href="#" >(仮)</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="#" >提出シフト管理</a>
+              <ul>
+                <li>
+                  <a href="#" >編集</a>
+                </li>
+                <li>
+                  <a href="#" >閲覧</a>
+                </li>
+                <li>
+                  <a href="#" >(仮)</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="#" >シフト閲覧</a>
+              <ul>
+                <li>
+                  <a href="#" >編集</a>
+                </li>
+                <li>
+                  <a href="#" >閲覧</a>
+                </li>
+                <li>
+                  <a href="#" >(仮)</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="#" >シフト作成</a>
+              <ul>
+                <li>
+                  <a href="#" >編集</a>
+                </li>
+                <li>
+                  <a href="#" >閲覧</a>
+                </li>
+                <li>
+                  <a href="#" >(仮)</a>
+                </li>
+              </ul>
             </li>
           </ul>
          </div>
