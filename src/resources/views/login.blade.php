5f5b2caf845bb63313ba4e66dcebaf0ccc2ad230
masao <masakiyama1218@outlook.jp>
2022-11-02 15:35:03 +0900

    lalabel ui

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -6,16 +6,29 @@
 </header>
 
 <body>
+
+
     <div class="loginWrapper">
-        <p class="information">ⓘ</p>
+        <div class="informationArea">
+            <p>ⓘ</p>
+            <div class="informationMessage">ユーザーIDとパスワードを忘れた方は管理者にお問い合わせください。</div>
+        </div>
         <p class="loginPane">ロ&nbsp;グ&nbsp;イ&nbsp;ン</p>
-        <label for="mailId">メール or ユーザーID&emsp;&emsp;</label>
+        <label for="mailId">*メール or ユーザーID&emsp;&emsp;</label>
         <input type="text" id="mailId" name="mailID" placeholder="Email OR UserId" size="25">
         <br />
         <br />
-        <label for="pass">パ&emsp;ス&emsp;ワ&emsp;ー&emsp;ド&nbsp;&ensp;&emsp;&emsp;</label>
+        <label for="pass">*パ&emsp;ス&emsp;ワ&emsp;ー&emsp;ド&nbsp;&ensp;&emsp;&emsp;</label>
         <input type="password" id="pass" name="pass" placeholder="パスワード" size="25">
+        <br />
+        <br />
+        <button onclick="history.back()" class="backButton">戻る</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="submit" value="ログイン" class="loginButton">
 
 
     </div>
+
+
+
+
 </body>
