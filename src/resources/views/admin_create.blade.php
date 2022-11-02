1d9b10f86d9cb543fdd00f9c09c6dddbb49cf5ca
hkr910 <2001kudou0902@gmail.com>
2022-11-02 15:26:06 +0900

    管理者基本情報登録画面、途中

--- src/resources/views/admin_create.blade.php
+++ src/resources/views/admin_create.blade.php
@@ -9,4 +9,28 @@
         <a class="rootsymbol"><</a>
         <a  class="root_location">タイトル(仮)</a>
     </div>
-    <img src="{{ asset('img_submit/submit.png') }}">
+    <img src="{{ asset('img_submit/submit.png') }}" class="img_create">
+    <div class="formbox">
+        <div class="inputWithIcon">
+            <input type="text" placeholder="氏名">
+          </div>
+          
+          <div class="inputWithIcon">
+            <input type="text" placeholder="メールアドレス">
+            <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+          </div>
+          
+          <div class="inputWithIcon">
+            <input type="text" placeholder="企業名">
+            <i class="fa fa-phone fa-lg fa-fw" aria-hidden="true"></i>
+          </div>     
+          <div class="inputWithIcon">
+            <input type="password" placeholder="パスワード">
+            <i class="fa fa-phone fa-lg fa-fw" aria-hidden="true"></i>
+          </div>  
+          <div class="inputWithIcon">
+            <input type="password" placeholder="パスワード確認">
+            <i class="fa fa-phone fa-lg fa-fw" aria-hidden="true"></i>
+          </div>  
+          
+    </div>
