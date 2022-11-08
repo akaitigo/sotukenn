13e3bfa613ab7e5882028ffc4c221a12817cc656
masao <masakiyama1218@outlook.jp>
2022-11-08 16:21:37 +0900

    controlloer

--- src/app/Http/Controllers/EmployeeController.php
+++ src/app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index(){
+        $employee = Employee::where('employee_id','2')->first();
+        
+        dd($employee);
+
+    }
 }
