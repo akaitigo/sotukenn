5e1817c8b606575d49679f8e6eba571bcb78b1e9
masao <masakiyama1218@outlook.jp>
2022-11-08 15:36:19 +0900

    login

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -1,96 +1,96 @@
 <link rel="stylesheet" href="/css/header.css" type="text/css">
-<header >
-      <div class="blandWrapper">
-        <a class="brand">M&emsp;A&emsp;R&emsp;U&emsp;O&emsp;K&emsp;U&emsp;N</a>
-      </div>
-      <div class="menuhyde">
-        <div class="menuHeaderWrap">
-          <ul class="menuclass" id="menu">
+<header>
+  <div class="blandWrapper">
+    <a class="brand">M&emsp;A&emsp;R&emsp;U&emsp;O&emsp;K&emsp;U&emsp;N</a>
+  </div>
+  <div class="menuhyde">
+    <div class="menuHeaderWrap">
+      <ul class="menuclass" id="menu">
+        <li>
+          <a href="#">カレンダー</a>
+          <ul>
             <li>
-              <a href="#" >カレンダー</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
+              <a href="#">編集</a>
             </li>
             <li>
-              <a href="#" >従業員管理</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
+              <a href="#">閲覧</a>
             </li>
             <li>
-              <a href="#" >通知管理</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" >提出シフト管理</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" >シフト閲覧</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" >シフト作成</a>
-              <ul>
-                <li>
-                  <a href="#" >編集</a>
-                </li>
-                <li>
-                  <a href="#" >閲覧</a>
-                </li>
-                <li>
-                  <a href="#" >(仮)</a>
-                </li>
-              </ul>
+              <a href="#">(仮)</a>
             </li>
           </ul>
-         </div>
-        
+        </li>
+        <li>
+          <a href="#">従業員管理</a>
+          <ul>
+            <li>
+              <a href="#">編集</a>
+            </li>
+            <li>
+              <a href="#">閲覧</a>
+            </li>
+            <li>
+              <a href="#">(仮)</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">通知管理</a>
+          <ul>
+            <li>
+              <a href="#">編集</a>
+            </li>
+            <li>
+              <a href="#">閲覧</a>
+            </li>
+            <li>
+              <a href="#">(仮)</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">提出シフト管理</a>
+          <ul>
+            <li>
+              <a href="#">編集</a>
+            </li>
+            <li>
+              <a href="#">閲覧</a>
+            </li>
+            <li>
+              <a href="#">(仮)</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">シフト閲覧</a>
+          <ul>
+            <li>
+              <a href="#">編集</a>
+            </li>
+            <li>
+              <a href="#">閲覧</a>
+            </li>
+            <li>
+              <a href="#">(仮)</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">シフト作成</a>
+          <ul>
+            <li>
+              <a href="#">編集</a>
+            </li>
+            <li>
+              <a href="#">閲覧</a>
+            </li>
+            <li>
+              <a href="#">(仮)</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+
 </header>
