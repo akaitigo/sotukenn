f64b191cf5377109ba27b31e75f684216c3cb0dc
hkr910 <2001kudou0902@gmail.com>
2022-11-10 14:18:50 +0900

    ログイン、登録のmodel、contorollerの追加、変更

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'enterprise' => $data['enterprise'],
             'password' => Hash::make($data['password']),
         ]);
     }
