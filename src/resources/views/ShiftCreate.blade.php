7c4eb13cdca27f0d08578b2cff05deedc2420f93
shomamama <saokrt1416@au.com>
2022-11-10 13:37:14 +0900

    route

---
+++ src/resources/views/ShiftCreate.blade.php
@@ -0,0 +1,2 @@
+<!-- シフト作成待ち時間 -->
+<meta http-equiv="refresh" content=" 7; url= {{ route('candidacyView') }}">  <!-- 間違ってるかも -->
--- src/resources/views/ShiftCreateOption.blade.php
+++
@@ -1,2 +0,0 @@
-<!-- シフト作成待ち時間 -->
-<meta http-equiv="refresh" content=" 7; url= {{ route('candidacyView') }}">  <!-- 間違ってるかも -->
