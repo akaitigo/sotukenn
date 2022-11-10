a7d83bb71265e0d3bd37d12522876a4a423a651e
masao <masakiyama1218@outlook.jp>
2022-11-10 17:00:05 +0900

    Parttim 中間テーブル関連　Jobがとれない

--- src/app/Http/Controllers/EmployeeController.php
+++ src/app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Models\Parttimer;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -10,6 +11,15 @@ class EmployeeController extends Controller
     public function index()
     {
         $employees = Employee::get();
-        return view('employeesManagement', compact('employees'));
+        $parttimers = Parttimer::all();
+        foreach ($parttimers as $parttimer) {
+            echo "<br/>{$parttimer->parttimer_name}の仕事たち：";
+        }
+
+        foreach ($parttimer->Jobs as $job) {
+            echo "{$job->job_id}";
+            echo "a";
+        }
+        return view('employeesManagement', compact('employees', 'parttimers'));
     }
 }
--- src/resources/views/employeesManagement.blade.php
+++ src/resources/views/employeesManagement.blade.php
@@ -1,8 +1,25 @@
 @include('header')
 
-<p>{{$employees}}</p>
 
-@foreach($employees as $emp)
-<p>{{$emp->employee_id}}</p>
-<p>{{$emp->employee_name}}</p>
-@endforeach
+<h2>従業員一覧</h2>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>pass</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($employees as $employee)
+        <tr>
+            <td>{{ $employee->employee_id}}</td>
+            <td>{{ $employee->employee_name }}</td>
+            <td>{{ $employee->employee_pass }}</td>
+
+        </tr>
+        @endforeach
+    </tbody>
+</table>
