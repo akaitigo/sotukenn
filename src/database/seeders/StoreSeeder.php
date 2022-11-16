83f300a37af47a5824c923e89ae86bb51e6fbc28
masao <masakiyama1218@outlook.jp>
2022-11-16 11:39:41 +0900

    DBtyousei

--- src/database/seeders/StoreSeeder.php
+++ src/database/seeders/StoreSeeder.php
@@ -16,7 +16,7 @@ class StoreSeeder extends Seeder
     public function run()
     {
         DB::table('stores')->insert([
-<<<<<<< HEAD
+
             'id' => '1',
             'store_name' => 'しょーまカンパニー',
             'workstarttime' => '10',
@@ -41,30 +41,7 @@ class StoreSeeder extends Seeder
             'workendtime' => '22',
             'submissionlimit' => '1',
             'vote' => false
-=======
-            'id'=>'1',
-            'store_name'=>'しょーまカンパニー',
-            'workstarttime'=>'10',
-            'workendtime'=>'22',
-            'vote'=>'0'
-        ]);
 
-        DB::table('stores')->insert([
-            'id'=>'2',
-            'store_name'=>'まるおカンパニー',
-            'workstarttime'=>'10',
-            'workendtime'=>'22',
-            'vote'=>'0'
         ]);
-
-        DB::table('stores')->insert([
-            'id'=>'3',
-            'store_name'=>'ひかるカンパニー',
-            'workstarttime'=>'10',
-            'workendtime'=>'22',
-            'vote'=>'0'
->>>>>>> 33caf6bea215bd58c0ee76244dd286973dfca225
-        ]);
-        
     }
 }
