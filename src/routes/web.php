df80749d099b78d74e70b782211f89d821d9c8fe
toku <tokuzawa0804yamagami@gmail.com>
2022-11-28 15:38:58 +0900

    javaコミットするお

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('title');
+    return view('ShiftCreate');
 });
 Route::get('home', function () {
     return view('test');
