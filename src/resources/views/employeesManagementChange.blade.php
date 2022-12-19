902b97247965b39e0ac8dfeab58038b1c32fbab2
masao <masakiyama1218@outlook.jp>
2022-12-20 00:01:02 +0900

    CSS調整

--- src/resources/views/employeesManagementChange.blade.php
+++ src/resources/views/employeesManagementChange.blade.php
@@ -29,83 +29,77 @@
     <tbody>
         <form method="post" action="{{route('employeesManegementUpdate')}}">
             @csrf
-        @foreach ($employees as $emp)
-        <tr>
-            <td>{{ $emp->id }}</td>
-            <td><input type="text" name="newEmpName" placeholder="{{ $emp->name }}"></td>
-            <td><input type="email" name="newEmpEmail" placeholder="{{$emp->email}}"></td>
-            <td><input type="text" name="newEmpAge" placeholder="{{$emp->age}}"></td>
-            <td><input type="text" name="newEmpWeight" placeholder="{{ $emp->weight }}"></td>
+            @foreach ($employees as $emp)
+            <tr>
 
-            <td>
-                @foreach($allJob as $alljob)
+                <td>{{ $emp->id }}</td>
 
-                <input type="checkbox" name="{{$loop->iteration}}" value="{{$loop->iteration}}" {{in_array($alljob->id, $jobcheck)? 'checked' : '' }}>
-                {{$alljob->name}}
-                {{-- {{$loop->iteration}}で現在のループ回数--}}
-                @endforeach
-
-            </td>
-
-            <td><input type="text" name="newEmpPassword" placeholder="Password"></td>
+                <td><input type="text" class="box" name="newEmpName" placeholder="{{ $emp->name }}"></td>
+                <td><input type="email" class="box" name="newEmpEmail" placeholder="{{$emp->email}}"></td>
+                <td><input type="text" class="box" name="newEmpAge" placeholder="{{$emp->age}}"></td>
+                <td><input type="text" class="box" name="newEmpWeight" placeholder="{{ $emp->weight }}"></td>
 
 
+
+                <td>
+                    @foreach($allJob as $alljob)
+                    <input type="checkbox" name="{{$loop->iteration}}" value="{{$loop->iteration}}" {{in_array($alljob->id, $jobcheck)? 'checked' : '' }}>
+                    {{$alljob->name}}
+                    {{-- {{$loop->iteration}}で現在のループ回数--}}
+                    @endforeach
+                </td>
+                <td><input type="text" class="box" name="newEmpPassword" placeholder="Password"></td>
                 <td><button type="submit" name="upDateId" value="{{$emp->id}}" class="updateButton">更　新</button></td>
         </form>
         <form method="post" action="{{route('employeesManagementDelete')}}" onsubmit="return DeleteCheck();">
             @csrf
             <td><button type="submit" name="delete" value="{{$emp->id}}" class="deleteButton">削　除</td>
         </form>
-
-
         </tr>
         @endforeach
     </tbody>
 
 
 
-@elseif($partChangeIden==true)
-        <tbody>
-            <form method="post" action="{{route('parttimersManegementUpdate')}}">
-        @foreach ($parttimers as $part)
-        <tr>
-            <td>{{ $part->id }}</td>
-            <td><input type="text" name="newPartName" placeholder="{{ $part->name }}"></td>
-            <td><input type="email" name="newPartEmail" placeholder="{{$part->email}}"></td>
-            <td><input type="text" name="newPartAge" placeholder="{{$part->age}}"></td>
-            <td><input type="text" name="newPartWeight" placeholder="{{ $part->weight }}"></td>
+    @elseif($partChangeIden==true)
+    <tbody>
+        <form method="post" action="{{route('parttimersManegementUpdate')}}">
+            @foreach ($parttimers as $part)
+            <tr>
+                <td>{{ $part->id }}</td>
+                <td><input type="text" name="newPartName" placeholder="{{ $part->name }}"></td>
+                <td><input type="email" name="newPartEmail" placeholder="{{$part->email}}"></td>
+                <td><input type="text" name="newPartAge" placeholder="{{$part->age}}"></td>
+                <td><input type="text" name="newPartWeight" placeholder="{{ $part->weight }}"></td>
 
-            <td>
+                <td>
 
-                @foreach($allJob as $alljob)
-                <input type="checkbox" name="{{$loop->iteration}}" value="{{$loop->iteration}}" {{in_array($alljob->id, $jobcheck)? 'checked' : '' }}>
-                {{$alljob->name}}
+                    @foreach($allJob as $alljob)
+                    <input type="checkbox" name="{{$loop->iteration}}" value="{{$loop->iteration}}" {{in_array($alljob->id, $jobcheck)? 'checked' : '' }}>
+                    {{$alljob->name}}
 
-                {{-- {{$loop->iteration}}で現在のループ回数--}}
-                @endforeach
-            </td>
+                    {{-- {{$loop->iteration}}で現在のループ回数--}}
+                    @endforeach
+                </td>
 
-            <td><input type="text" name="newPartPass" placeholder="password"></td>
+                <td><input type="text" name="newPartPass" placeholder="password"></td>
 
 
                 @csrf
                 <td><button type="submit" name="upDateId" value="{{$part->id}}" class="updateButton">更新</button></td>
-            </form>
+        </form>
 
-            <form method="post" action="{{route('partManagementDelete')}}" onsubmit="return DeleteCheck();">
-                @csrf
-                <td><button type="submit" name="delete" value="{{$emp->id}}" class="deleteButton">削　除</td>
-            </form>
+        <form method="post" action="{{route('partManagementDelete')}}" onsubmit="return DeleteCheck();">
+            @csrf
+            <td><button type="submit" name="delete" value="{{$emp->id}}" class="deleteButton">削　除</td>
+        </form>
         </tr>
         @endforeach
     </tbody>
     @endif
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+    <br />
+    <br />
+    <br />
+    <br />
 
 </table>
-
-
-
