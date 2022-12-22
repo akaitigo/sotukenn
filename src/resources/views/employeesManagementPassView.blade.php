76732eccadae3bbdfd2f47254745a2a40a942b21
hkr910 <2001kudou0902@gmail.com>
2022-12-22 14:17:52 +0900

    従業員管理画面pagination作成

--- src/resources/views/employeesManagementPassView.blade.php
+++ src/resources/views/employeesManagementPassView.blade.php
@@ -12,7 +12,7 @@
     <title>従業員管理</title>
     <div id='container'>
         <div class='widget'>
-            <div id='正社員' class="tab-content" href="#tab1">
+            <div id='正社員' class="tab-content" href="?tab1">
 
                 @if (!$employees->isEmpty())
 
@@ -31,9 +31,44 @@
 
                             </tr>
                         </thead>
+                        <?php $count = 0; ?>
+                        @foreach ($employees as $emp)
+                            <?php $count = $count + 1; ?>
+                        @endforeach
+                        <?php
+                        $pagination = ceil($count / 4);
+                        if (isset($_GET['page'])) {
+                            $page = (int) $_GET['page']; //ページの取得
+                        } else {
+                            $page = 1; //始めのページ
+                        }
+                        
+                        // スタートのポジションを計算する
+                        if ($page > 1) {
+                            // 例：２ページ目の場合は、(2 × 4) - 4 = 4
+                            $start = $page * 4 - 4;
+                        } else {
+                            $start = 0;
+                        }
+                        $start_loop = $start + 1; //1ページの表示の始め　例8件目からとか
+                        $count_loop = 1; //現在の表示件数
+                        $end_loop = 4 * $page; //1ページあたりの表示の終わり
+                        
+                        ?>
 
                         <tbody>
                             @foreach ($employees as $emp)
+                                {{-- 表示件数の処理 --}}
+                                @if ($start_loop > $count_loop)
+                                    <?php
+                                    $count_loop = $count_loop + 1;
+                                    continue; ?>
+                                @endif
+                                @if ($end_loop < $count_loop)
+                                    <?php
+                                    $count_loop = $count_loop + 1;
+                                    continue; ?>
+                                @endif
                                 <tr>
                                     <td>{{ $emp->id }}</td>
                                     <td>{{ $emp->email }}</td>
@@ -67,12 +102,20 @@
                                         </td>
                                     </form>
                                 </tr>
+                                <?php
+                                $count_loop = $count_loop + 1;
+                                ?>
                             @endforeach
                         </tbody>
                     </table>
                 @endif
+                <?php for ($x=1; $x <= $pagination ; $x++) { ?>
+                <a class='pagetab' href="?page=<?php echo $x; ?>"><?php echo $x; ?></a>
+                <?php } ?>
             </div>
         </div>
+
+
 
         <div class='widget'>
             <div id='アルバイト' class="tab-content">
@@ -93,9 +136,28 @@
                                 <th>change</th>
                             </tr>
                         </thead>
+                        <?php $count = 0; ?>
+                        @foreach ($employees as $emp)
+                            <?php $count = $count + 1; ?></a>
+                        @endforeach
+                        <?php
+                        $pagination = ceil($count / 4);
+                        $count_loop = 1; //現在の表示件数
+                        ?>
                         <tbody>
                             @foreach ($parttimers as $part)
-                                <tr>
+                                {{-- 表示件数の処理 --}}
+                                @if ($start_loop > $count_loop)
+                                    <?php
+                                    $count_loop = $count_loop + 1;
+                                    continue; ?>
+                                @endif
+                                @if ($end_loop < $count_loop)
+                                    <?php
+                                    $count_loop = $count_loop + 1;
+                                    continue; ?>
+                                @endif
+                                <tr id="<?php echo $count_loop; ?>">
                                     <td>{{ $part->id }}</td>
                                     <td>{{ $part->email }}</td>
                                     <td>{{ $part->name }}</td>
@@ -111,8 +173,8 @@
                                     @if (!is_null($part->lineUserId))
                                         <form method="get" action="{{ route('partMessagessent') }}">
                                             @csrf
-                                            <td><button type="submit" name="noticeId" value="{{ $part->id }}"
-                                                    class="noticeButton">通知</button></td>
+                                            <td id="<?php echo $count_loop; ?>"><button type="submit" name="noticeId"
+                                                    value="{{ $part->id }}" class="noticeButton">通知</button></td>
                                         </form>
                                     @endif
                                     @if (is_null($part->lineUserId))
@@ -126,10 +188,22 @@
                                                 value="{{ $part->id }}">変　更</button></td>
                                     </form>
                                 </tr>
+                                <?php
+                                $count_loop = $count_loop + 1;
+                                ?>
                             @endforeach
                         </tbody>
                     </table>
                 @endif
+                <?php for ($x=1; $x <= $pagination ; $x++) { ?>
+                <a class='pagetab2_<?php echo $x; ?>' href="?tab=2&page=<?php echo $x; ?>"><?php echo $x; ?></a>
+                <?php } ?>
+            </div>
+
+        </div>
+        {{-- なぜかこれが無いとアルバイトが最初にでない --}}
+        <div class='widget3' >
+            <div id='アルバイト' class="tab-content">
             </div>
         </div>
 
@@ -139,3 +213,5 @@
 
 <script type="text/javascript" src="/js/tab.js"></script>
 <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+
+
