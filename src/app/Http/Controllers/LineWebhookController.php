68738b61788d70c93581bc6a3955999042ee42a7
masao <masakiyama1218@outlook.jp>
2023-01-11 00:56:22 +0900

    1/11

--- src/app/Http/Controllers/LineWebhookController.php
+++ src/app/Http/Controllers/LineWebhookController.php
@@ -63,23 +63,23 @@ class LineWebhookController extends Controller
             } else if ($inputText === '情報共有') {
                 $employeeNullCheck = Employee::where('lineUserId', '=', $event['source']['userId'])->get();
                 $partNullCheck = Parttimer::where('lineUserId', '=', $event['source']['userId'])->get();
-                $empNullBool=false;
-                $partNullBool=false;
+                $empNullBool = false;
+                $partNullBool = false;
                 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('情報共有ですね！情報共有では、従業員各位に情報を共有することが可能です。');
                 $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
                 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("※注意：MARUOKUNに表示される期間は最大15日間です。\n通知は一度しか行われません。他者を不快にさせるような通知は行わないようお願いいたします。\n本通知は管理者が確認することができます。");
                 $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('下記のテンプレートの例を削除して必要事項の記入をお願いします。');
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('下記のリンクより登録、参照を行うことができます。');
                 $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
-                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("NOTICE\n表示期間：例10（最大15日まで）\n｜通知対象：例キッチン(全体の場合は’ALL’)\n｜通知内容：例〇時に▽があります。");
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("route('informationShare)");
                 $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
                 break;
-            }else if(strpos($inputText, 'NOTICE') !== false){//情報共有
+            } else if (strpos($inputText, 'NOTICE') !== false) { //情報共有
 
 
 
 
-            }else if (strpos($inputText, '@') !== false) { //メールアドレスか検査
+            } else if (strpos($inputText, '@') !== false) { //メールアドレスか検査
                 $forcheck = true;
                 $employeeNullCheck = Employee::where('email', '=', $inputText)->get();
                 $partNullCheck = Parttimer::where('email', '=', $inputText)->get();
@@ -139,7 +139,7 @@ class LineWebhookController extends Controller
                     $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
                     break;
                 }
-            }else{
+            } else {
                 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("コマンドが確認できませんでした。");
                 $response = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
                 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ヘルプと送信したら利用できるコマンドを確認することができます。\nスマホでご利用の方は、下部のメニューから操作をお願いします");
