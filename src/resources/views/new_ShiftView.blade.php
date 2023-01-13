161fd20d5d95cc7ade5dc556339087e7afb2a379
hkr910 <2001kudou0902@gmail.com>
2023-01-13 12:56:52 +0900

    シフト表

--- src/resources/views/new_ShiftView.blade.php
+++ src/resources/views/new_ShiftView.blade.php
@@ -5,29 +5,41 @@
 <title>シフト閲覧</title>
 @include('new_header')
 <div id="scale">
-<div id='container'>
-            <div class='widget'>
-                <div class="tab-content" href="#tab1">
+    <div id='container'>
+        <div class="colorbox_flex">
+            <div class="colorbox empcolor">
+            </div>
+            <div class="colorbox_text">
+                従業員
+            </div>
+            <div class="colorbox partcolor" id="makeImg">
+            </div>
+            <div class="colorbox_text">
+                アルバイト
+            </div>
+        </div>
+        <div class='widget'>
+            <div class="tab-content" href="#tab1">
                 <table class="month_table">
-                            <caption>１ 月</caption>
+                    <caption>１ 月</caption>
                 </table>
-                    <div class="scrollbox">
-                        <table class="compshift_table">
+                <div class="scrollbox">
+                    <table class="compshift_table">
                         <thead class="thead">
                             <th class="thname">&nbsp;名前&nbsp;</th>
-                            @for($i= 1;$i <= 31; $i++)
-                                <?php 
-                                    $timestamp = mktime(0,0,0,1,$i,2023);
-                                    $date = date('w', $timestamp);
+                            @for ($i = 1; $i <= 31; $i++)
+                                <?php
+                                $timestamp = mktime(0, 0, 0, 1, $i, 2023);
+                                $date = date('w', $timestamp);
                                 ?>
-                                @if($i == 9)
-                                    <th class="holiday">{{ $i }} 日({{$week[$date]}})</th>
+                                @if ($i == 9)
+                                    <th class="holiday">{{ $i }} 日({{ $week[$date] }})</th>
                                 @elseif($date == 6)
-                                    <th class="saturday">{{ $i }} 日({{$week[$date]}})</th>
+                                    <th class="saturday">{{ $i }} 日({{ $week[$date] }})</th>
                                 @elseif($date == 0)
-                                    <th class="sunday">{{ $i }} 日({{$week[$date]}})</th>
+                                    <th class="sunday">{{ $i }} 日({{ $week[$date] }})</th>
                                 @else
-                                    <th>{{ $i }} 日({{$week[$date]}})</th>
+                                    <th>{{ $i }} 日({{ $week[$date] }})</th>
                                 @endif
                             @endfor
                             <th class="workday">労働日数</th>
@@ -35,12 +47,12 @@
                         </thead>
                         <tbody>
                             <?php
-                                $empnameid = 0;
-                                $partnameid = 0;
-                                $rowcolor = 0;
-                                $i = 0;
+                            $empnameid = 0;
+                            $partnameid = 0;
+                            $rowcolor = 0;
+                            $i = 0;
                             ?>
-                            @foreach($completeshift as $compshift)
+                            @foreach ($completeshift as $compshift)
                                 <tr>
                                 @if($compshift->emppartid == $loginid && $compshift->judge == $empjudge)
                                     @if($compshift->judge)
@@ -68,11 +80,11 @@
                                     @endif
                                 @endif
 
-                                    
-                                    @if($compshift->emppartid == $loginid && $compshift->judge == $empjudge)
-                                        @for($day= 1; $day <= 31; $day++)
-                                        <?php $hentai="day".$day?>
-                                            <td class="loginrow">{{$compshift->$hentai}}</td>
+
+                                    @if ($compshift->emppartid == $loginid && $compshift->judge == $empjudge)
+                                        @for ($day = 1; $day <= 31; $day++)
+                                            <?php $hentai = 'day' . $day; ?>
+                                            <td class="loginrow">{{ $compshift->$hentai }}</td>
                                         @endfor
                                     @elseif($rowcolor % 2 == 0)
                                         @for($day= 1; $day <= 31; $day++)
@@ -94,27 +106,27 @@
                                         @endfor
                                     @endif
 
-                                    @if($compshift->emppartid == $loginid && $compshift->judge == $empjudge)
-                                        <td class="loginrow">{{$Staffworkdays[$i]}}</td>
-                                        <td class="loginrow">{{$StaffTimes[$i]}}</td>
+                                    @if ($compshift->emppartid == $loginid && $compshift->judge == $empjudge)
+                                        <td class="loginrow">{{ $Staffworkdays[$i] }}</td>
+                                        <td class="loginrow">{{ $StaffTimes[$i] }}</td>
                                     @elseif($rowcolor % 2 == 0)
-                                        <td class="gusurow">{{$Staffworkdays[$i]}}</td>
-                                        <td class="gusurow">{{$StaffTimes[$i]}}</td>
+                                        <td class="gusurow">{{ $Staffworkdays[$i] }}</td>
+                                        <td class="gusurow">{{ $StaffTimes[$i] }}</td>
                                     @else
-                                        <td class="kisurow">{{$Staffworkdays[$i]}}</td>
-                                        <td class="kisurow">{{$StaffTimes[$i]}}</td>
+                                        <td class="kisurow">{{ $Staffworkdays[$i] }}</td>
+                                        <td class="kisurow">{{ $StaffTimes[$i] }}</td>
                                     @endif
                                 </tr>
                                 <?php $rowcolor++;
-                                      $i++; ?>
+                                $i++; ?>
                             @endforeach
                         </tbody>
-                        </table>
-                    </div>
-                    @if($loginid != 0)
-                        {{$staffshiftcover}}
-                    @endif
+                    </table>
                 </div>
+                @if ($loginid != 0)
+                    {{ $staffshiftcover }}
+                @endif
             </div>
         </div>
+    </div>
 </div>
