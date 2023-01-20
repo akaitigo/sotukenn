5f884010d0b27d4e3f14946eaa3bce236cb3777c
hkr910 <2001kudou0902@gmail.com>
2023-01-20 13:40:06 +0900

    名前変更

--- src/resources/views/emp_calendar_show.blade.php
+++ src/resources/views/emp_calendar_show.blade.php
@@ -26,7 +26,7 @@ $all_data = array_merge($event, $day);
 $json_data = json_encode($all_data);
 ?>
 @include('new_header')
-<link rel="stylesheet" href="/css/calendar.css" type="text/css">
+<link rel="stylesheet" href="/css/test_calendar.css" type="text/css">
 <link rel="stylesheet" href="/css/scale.css" type="text/css">
 <title>カレンダー</title>
 {{-- @include('new_header') --}}
@@ -41,4 +41,4 @@ $json_data = json_encode($all_data);
 {{-- <script async src="https://js.cybozu.com/momentjs/2.27.0/moment.min.js"></script> --}}
 {{-- <script async src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js"></script> --}}
-<script type="text/javascript" src="/js/calendar.js"></script>
+<script type="text/javascript" src="/js/test_calendar.js"></script>
